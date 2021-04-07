81484e5889839c600ec022ebb28d77224858539b
stefanosclano <stefanosclano@gmil.com>
2021-04-07 18:12:07 +0200

    Clean up

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -7,49 +7,41 @@ from confluent_kafka import avro
 from models.producer import Producer
 from models.turnstile_hardware import TurnstileHardware
 
-
 logger = logging.getLogger(__name__)
 
 
 class Turnstile(Producer):
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
+
     value_schema = avro.load(
-       f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+        f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
     )
 
     def __init__(self, station):
         """Create the Turnstile"""
+
         super().__init__(
-            topic_name=f"org.chicago.cta.station.turnstile",
+            "org.chicago.cta.turnstile",
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
-            num_partitions=5,
+            num_partitions=1,
             num_replicas=1,
         )
-            
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
 
     def run(self, timestamp, time_step):
         """Simulates riders entering through the turnstile."""
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
-        
-        logger.info(
-            "%s riders have entered station %s at %s",
-            num_entries,
-            self.station.name,
-            timestamp.isoformat(),
-        )
-        
         for _ in range(num_entries):
             self.producer.produce(
                 topic=self.topic_name,
                 key_schema=self.key_schema,
-                key={"timestamp": self.time_millis()},
                 value_schema=self.value_schema,
+                key={"timestamp": self.time_millis()},
                 value={
-                    "station_id": self.station.station_id,
                     "station_name": self.station.name,
+                    "station_id": self.station.station_id,
                     "line": self.station.color.name
-                },
+                }
             )
