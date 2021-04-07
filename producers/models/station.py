da948d83c269c9e9a7d4096d32ae000eb5c417a5
stefanosclano <stefanosclano@gmil.com>
2021-04-07 10:15:28 +0200

    Updated formatting

--- producers/models/station.py
+++ producers/models/station.py
@@ -7,7 +7,6 @@ from confluent_kafka import avro
 from models import Turnstile
 from models.producer import Producer
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -18,17 +17,17 @@ class Station(Producer):
 
     def __init__(self, station_id, name, color, direction_a=None, direction_b=None):
         self.name = name
-        
+
         station_name = (
             self.name.lower()
-            .replace("/", "_and_")
-            .replace(" ", "_")
-            .replace("-", "_")
-            .replace("'", "")
+                .replace("/", "_and_")
+                .replace(" ", "_")
+                .replace("-", "_")
+                .replace("'", "")
         )
 
         topic_name = f"org.chicago.cta.station.arrivals.{station_name}"
-        
+
         super().__init__(
             topic_name,
             key_schema=Station.key_schema,
@@ -44,7 +43,6 @@ class Station(Producer):
         self.a_train = None
         self.b_train = None
         self.turnstile = Turnstile(self)
-
 
     def run(self, train, direction, prev_station_id, prev_direction):
         """Simulates train arrivals at this station"""
@@ -65,14 +63,15 @@ class Station(Producer):
         )
 
     def __str__(self):
-        return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
-            self.station_id,
-            self.name,
-            self.a_train.train_id if self.a_train is not None else "---",
-            self.dir_a.name if self.dir_a is not None else "---",
-            self.b_train.train_id if self.b_train is not None else "---",
-            self.dir_b.name if self.dir_b is not None else "---",
-        )
+        return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | " \
+               "departing to {:<30} | ".format(
+                                                self.station_id,
+                                                self.name,
+                                                self.a_train.train_id if self.a_train is not None else "---",
+                                                self.dir_a.name if self.dir_a is not None else "---",
+                                                self.b_train.train_id if self.b_train is not None else "---",
+                                                self.dir_b.name if self.dir_b is not None else "---",
+                                                )
 
     def __repr__(self):
         return str(self)
