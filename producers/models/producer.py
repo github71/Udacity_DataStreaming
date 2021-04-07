674634a0431b75e13dc21dd065304f507874469c
stefanosclano <stefanosclano@gmil.com>
2021-04-07 10:14:34 +0200

    Updated formatting, and made def time_millis(): a @staticmethod

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -1,7 +1,6 @@
-"""Producer base-class providing common utilites and functionality"""
+"""Producer base-class providing common utilities and functionality"""
 import logging
 import time
-
 
 from confluent_kafka import avro
 from confluent_kafka.admin import AdminClient, NewTopic
@@ -17,12 +16,12 @@ class Producer:
     existing_topics = set([])
 
     def __init__(
-        self,
-        topic_name,
-        key_schema,
-        value_schema=None,
-        num_partitions=1,
-        num_replicas=1,
+            self,
+            topic_name,
+            key_schema,
+            value_schema=None,
+            num_partitions=1,
+            num_replicas=1,
     ):
         """Initializes a Producer object with basic settings"""
         self.topic_name = topic_name
@@ -30,14 +29,14 @@ class Producer:
         self.value_schema = value_schema
         self.num_partitions = num_partitions
         self.num_replicas = num_replicas
-        
-        logger.info(f"intialising topic %s ", topic_name)
-        
+
+        logger.info(f"initialising topic %s ", topic_name)
+
         self.broker_properties = {
             "bootstrap.servers": "PLAINTEXT://localhost:9092",
             "schema.registry.url": "http://localhost:8081",
         }
-                
+
         # If the topic does not already exist, try to create it
         if self.topic_name not in Producer.existing_topics:
             self.create_topic()
@@ -55,21 +54,21 @@ class Producer:
         client = AdminClient(
             {"bootstrap.servers": self.broker_properties["bootstrap.servers"]}
         )
-        
+
         topic_metadata = client.list_topics(timeout=5)
-        
+
         if self.topic_name in set(
                 t.topic for t in iter(topic_metadata.topics.values())
         ):
             logger.info("not recreating existing topic %s", self.topic_name)
-            return        
-        
+            return
+
         logger.info(f"creating new topic %s ", self.topic_name)
-        
-        topic = NewTopic(self.topic_name, 
-            num_partitions=self.num_partitions, 
-            replication_factor=self.num_replicas,
-        )
+
+        topic = NewTopic(self.topic_name,
+                         num_partitions=self.num_partitions,
+                         replication_factor=self.num_replicas,
+                         )
 
         client.create_topics([topic])
 
@@ -79,6 +78,7 @@ class Producer:
             logger.info("closing producer...")
             self.producer.flush()
 
-    def time_millis(self):
+    @staticmethod
+    def time_millis():
         """Use this function to get the key for Kafka Events"""
         return int(round(time.time() * 1000))
